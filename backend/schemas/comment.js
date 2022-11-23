cd3320f37b557ef4631dced6ee20ac96ef99bcd2
Hopp Murithi <hoppmurithi@gmail.com>
2022-11-23 13:47:17 +0300

    Create  sanity schema

---
+++ backend/schemas/comment.js
@@ -0,0 +1,14 @@
+export default{
+    name:'comment',
+    title:'Comment',
+    type:'document',
+    fields:[{
+        name:'postedBy',
+        title:'PostedBy',
+        type:'postedBy'
+    },{
+        name:'comments',
+        title:'Comments',
+        type:'string'
+    }]
+}
---
+++ backend/schemas/pin.js
@@ -0,0 +1,52 @@
+export default {
+    name:'pin',
+    title:'Pin',
+    type:'document',
+fields: [{
+    name:'title',
+    title:'Title',
+    type:'string'
+},
+{
+    name:'about',
+    title:'About',
+    type:'string'
+},
+{
+    name:'destination',
+    title:'Destination',
+    type:'url'
+},
+{
+    name:'category',
+    title:'category',
+    type:'url'
+},
+{
+    name:'image',
+    title:'Image',
+    type:'string',
+    options:{
+        hotspot:'true'
+    }
+},{
+    name:'userId',
+    title:'UserId',
+    type:'string'
+},{
+    name:'postedBy',
+    title:'PostedBy',
+    type:'postedBy'
+},{
+    name:'save',
+    title:'Save',
+    type:'array',
+    of:[{type:'save'}]
+},{
+    name:'comments',
+    title:'Comments',
+    type:'array',
+    of:[{type:'comment'}]
+}
+]
+}

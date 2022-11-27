6dac8a21449ba58afb71213db1ac811f8ab8b161
Hopp Murithi <hoppmurithi@gmail.com>
2022-11-27 20:50:56 +0300

    Add google auth into app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import {BrowserRouter as Router} from 'react-router-dom'
 import './index.css';
 import App from './App';
@@ -8,10 +9,15 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <Router>
+  
+  
+<Router>
+<GoogleOAuthProvider clientId="118320632153-8evj6t6uhifq0itaccek70vj6ul7tdoi.apps.googleusercontent.com" > 
    <App />
+  </GoogleOAuthProvider>;
   </Router>
-   
+  
+ 
   </React.StrictMode>
 );
 

8ff0720333c6f74b2b11548612b2fa92528df879
Hopp Murithi <hoppmurithi@gmail.com>
2022-12-21 14:21:42 +0300

    Update route path for Pins component

--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -23,7 +23,7 @@ const query = userQuery(userInfo?.sub)
 
 client.fetch(query)
 .then((data)=>{ 
-  //console.log(data)  
+  console.log(data)  
   setUser(data[0]);
 
 })
@@ -37,36 +37,36 @@ scrollRef.current.scrollTo(0,0)
 
   return (
     <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out'>
-    <div className='hidden md:flex h-screen flex-initial'>
-    <Sidebar user={user && user}/>
-    </div>
+     <div className='hidden md:flex h-screen flex-initial'>
+      <Sidebar user={user && user}/>
+     </div>
   
     <div className='flex md:hidden flex-row'>
-    <div className='p-2 w-full flex flex-row justify-between items-center shadow-md'>
-    <HiMenu fontSize={40} className='cursor-pointer' onClick={()=>{setToggleSidebar(true)}}/>
-    <Link to='/'>
-<img src={logo} alt='logo' className='w-10'/>
-    </Link>
-    <Link to={`user-profile/${user?._id}`}>
-<img src={user?.image} alt='user' className=' block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0 object-cover rounded-full h-[50px] w-[50px]'/>
-    </Link>
-    </div>
+     <div className='p-2 w-full flex flex-row justify-between items-center shadow-md'>
+        <HiMenu fontSize={40} className='cursor-pointer' onClick={()=>{setToggleSidebar(true)}}/>
+       <Link to='/'>
+         <img src={logo} alt='logo' className='w-10'/>
+       </Link>
+       <Link to={`user-profile/${user?._id}`}>
+        <img src={user?.image} alt='user' className=' block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0 object-cover rounded-full h-[50px] w-[50px]'/>
+       </Link>
+     </div>
     
-    {ToggleSidebar && (
+     {ToggleSidebar && (
       <div className='fixed w-4/5 h-screen bg-white overflow-y-auto shadow-md z-10 animate-slide-in'>
-<div className='absolute w-full flex justify-end items-center p-2'>
-  <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={()=>{setToggleSidebar(false)}}/>
-</div>
-<Sidebar user={user && user} closeToggle={setToggleSidebar}/>
+       <div className='absolute w-full flex justify-end items-center p-2'>
+         <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={()=>{setToggleSidebar(false)}}/>
+       </div>
+       <Sidebar user={user && user} closeToggle={setToggleSidebar}/>
       </div>
-    )}  
+     )}  
     </div>
    
-    <div className='pb-2 h-screen flex-1 overflow-y-scroll' ref={scrollRef}>
-      <Routes>
-        <Route path='user-profile/:userId' element={<UserProfile/>}/>
-        <Route path='/*' element={<Pins user={user && user}/>}/>
-      </Routes>
+     <div className='pb-2 h-screen flex-1 overflow-y-scroll' ref={scrollRef}>
+       <Routes>
+         <Route path='user-profile/:userId' element={<UserProfile/>}/>
+         <Route path='/*' element={<Pins user={user && user}/>}/>
+       </Routes>
     </div>
     </div>
   )

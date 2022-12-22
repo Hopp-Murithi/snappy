4588428d03ec18fb278f6f2fde1ba5885211827c
Hopp Murithi <hoppmurithi@gmail.com>
2022-12-22 22:23:17 +0300

    Create page loader

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Vortex} from 'react-loader-spinner';
+
+const Spinner = ({message}) => {
+  return (
+    <div className='flex flex-col justify-center items-center w-full h-full'>
+      
+<Vortex
+  visible={true}
+  height="80"
+  width="80"
+  ariaLabel="vortex-loading"
+  wrapperStyle={{}}
+  wrapperClass="vortex-wrapper"
+  colors={['red', 'green', 'blue', 'yellow', 'orange', 'purple','black']}
+/>
+<p className='text-center text-large px-2'>{message}</p>
+    </div>
+  )
+}
+
+export default Spinner

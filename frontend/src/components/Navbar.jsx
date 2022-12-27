ddc4584a978933fc73c69934ab9142e3ab1cc923
Hopp Murithi <hoppmurithi@gmail.com>
2022-12-27 13:41:21 +0300

    Format document for readability

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -21,11 +21,18 @@ const Navbar = ({ searchTerm, setSearchTerm, user }) => {
         />
       </div>
       <div className="flex gap-3">
-        <Link to={`user-profile/${user?._id}`} className='hidden md:block'>
-          <img src={user.image} alt='user-image'className="w-14 h-12 rounded-lg"/>
+        <Link to={`user-profile/${user?._id}`} className="hidden md:block">
+          <img
+            src={user.image}
+            alt="user-image"
+            className="w-14 h-12 rounded-lg"
+          />
         </Link>
-        <Link to='create-pin' className='bg-black text-white rounded-lg w-12 h-14 md:w-14 md:h-12 flex justify-center items-center'>
-         <IoMdAdd/>
+        <Link
+          to="create-pin"
+          className="bg-black text-white rounded-lg w-12 h-14 md:w-14 md:h-12 flex justify-center items-center"
+        >
+          <IoMdAdd />
         </Link>
       </div>
     </div>
